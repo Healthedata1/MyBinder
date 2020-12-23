d6b15d70e992d9e6849510e2e8279597b9e33f90
Healthedata1 <ehaas@healthedatainc.com>
2020-12-23 02:25:09 -0800

    prep for new test data

--- patient_lists/FHIR_templates.py
+++ patient_lists/FHIR_templates.py
@@ -55,17 +55,19 @@ example_org = {
 qr_template = {
   "resourceType": "QuestionnaireResponse",
   "meta": {
+    "lastUpdated": "2020-12-22T21:43:46-08:00",
     "source": "Health_eData_Inc",
+    "versionId": "1",
     "tag": [
       {
         "code": "2020-Sep"
       },
       {
-        "code": "lformsVersion: 25.1.2"
+        "code": "2021-Jan"
       }
     ]
   },
-  "questionnaire": "http://registry.fhir.org/argonaut/Questionniare/argo-pl-q1",
+  "questionnaire": "http://www.fhir.org/guides/argonaut/patient-list/Questionniare/argo-pl-q2",
   "status": "completed",
   "subject": {
     "reference": "Patient/12345",
@@ -74,61 +76,165 @@ qr_template = {
   "authored": "2020-09-25T00:11:54.322Z",
   "item": [
     {
-      "linkId": "dob",
-      "text": "DOB",
+      "linkId": "q1",
+      "text": "COVID Contact",
       "answer": [
         {
-          "valueString": "6/19/1964"
+          "valueBoolean": False
         }
       ]
     },
     {
-      "linkId": "sex",
-      "text": "Sex",
+      "linkId": "q2",
+      "text": "Age/Legal Sex",
       "answer": [
         {
-          "valueString": "M"
+          "valueString": "56/Male"
         }
       ]
     },
     {
-      "linkId": "admit-date",
-      "text": "Admission Datejson",
+      "linkId": "q3",
+      "text": "SAPS II Score",
       "answer": [
         {
-          "valueString": "09/24/2020"
+          "valueInteger": 80
         }
       ]
     },
     {
-      "linkId": "rel-encounter-id",
-      "text": "elated Encounter (e.g. admission Enounter) as id only",
+      "linkId": "q4",
+      "text": "SAPS II Score change",
       "answer": [
         {
-          "valueString": "1234"
+          "valueInteger": 12
         }
       ]
     },
     {
-      "linkId": "rel-encounter-ref",
-      "text": "Related Encounter (e.g. admission Enounter) as FHIR reference",
+      "linkId": "q5",
+      "text": "SOFA Score",
       "answer": [
         {
-          "valueReference": {
-            "reference": "Encounter/foo",
-            "display": "Related Encounter (e.g. admission Enounter) as FHIR reference"
-          }
+          "valueInteger": 12
         }
       ]
     },
     {
-      "linkId": "rel-encounter-uri",
-      "text": "Related Encounter (e.g. admission Encounter) as uri",
+      "linkId": "q6",
+      "text": "Unit",
       "answer": [
         {
-          "valueUri": "http://example.org/data/1234"
+          "valueString": "ICU"
         }
       ]
     }
   ]
 }
+
+appt_template = {
+  "resourceType" : "Appointment",
+  "id" : "pl-appt",
+  "status" : "booked",
+  "serviceType" : [
+    {
+      "coding" : [
+        {
+          "system" : "http://fhir.org/guides/argonaut-scheduling/CodeSystem/visit-type",
+          "code" : "117",
+          "display" : "Emergency Medical"
+        }
+      ],
+      "text" : "Emergency Medical"
+    }
+  ],
+  "specialty" : [
+    {
+      "coding" : [
+        {
+          "system" : "http://snomed.info/sct",
+          "code" : "408478003",
+          "display" : "Critical care medicine"
+        }
+      ],
+      "text" : "Critical care medicine"
+    }
+  ],
+  "appointmentType" : {
+    "coding" : [
+      {
+        "system" : "http://terminology.hl7.org/CodeSystem/v2-0276",
+        "code" : "EMERGENCY"
+      }
+    ],
+    "text" : "Routine"
+  },
+  "start" : "2020-12-21T14:30:00-08:00",
+  "end" : "2020-12-21T15:00:00-08:00",
+  "participant" : [
+        {
+      "actor" : {
+        "display" : "Patient X"
+      },
+      "required" : "required",
+      "status" : "accepted"
+    },
+    {
+      "actor" : {
+        "display" : "Dr Y"
+      },
+      "required" : "required",
+      "status" : "accepted"
+    },
+    {
+      "actor" : {
+        "display" : "Napa Office"
+      },
+      "required" : "required",
+      "status" : "accepted"
+    }
+  ]
+}
+my_q = {'id': 'argo-pl-q2',
+ 'meta': {'lastUpdated': '2020-05-20T15:46:00.964+00:00',
+  'source': '#eddpbEX0FzlvCJ4u',
+  'versionId': '1',
+  'profile': ['http://hl7.org/fhir/uv/sdc/StructureDefinition/sdc-questionnaire'],
+  'tag': [{'code': '2020-Sep'}, {'code': '2021-Jan'}]},
+ 'date': '2016-04-14',
+ 'item': [{'linkId': 'q1',
+   'repeats': False,
+   'required': False,
+   'text': 'COVID Contact',
+   'type': 'boolean'},
+  {'linkId': 'q2',
+   'repeats': False,
+   'required': True,
+   'text': 'Age/Legal Sex',
+   'type': 'string'},
+  {'linkId': 'q3',
+   'repeats': False,
+   'required': False,
+   'text': 'SAPS II Score',
+   'type': 'integer'},
+  {'linkId': 'q4',
+   'repeats': False,
+   'required': False,
+   'text': 'SAPS II Score change',
+   'type': 'integer'},
+  {'linkId': 'q5',
+   'repeats': False,
+   'required': False,
+   'text': 'SOFA Score',
+   'type': 'integer'},
+  {'linkId': 'q6',
+   'repeats': False,
+   'required': False,
+   'text': 'Unit',
+   'type': 'string'}],
+ 'name': 'Patient-List-columns',
+ 'status': 'draft',
+ 'subjectType': ['Patient'],
+ 'title': 'Patient List columns',
+ 'url': 'Questionnaire/patient-list-columns',
+ 'resourceType': 'Questionnaire'}
